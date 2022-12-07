a086c57cb38600677e2ae5567d64e91cf9d58581
usuario <usuario@T4-16>
2022-12-07 19:46:26 +0100

    Commit de Hasta Luego

--- examenEntorno/src/examenEntorno/main.java
+++ examenEntorno/src/examenEntorno/main.java
@@ -3,6 +3,6 @@ package examenEntorno;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola me llamo Sergio"); 
+		System.out.println("Hola me llamo");
 	}
 }
