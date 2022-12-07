c2af647626ac7f0390931aff5be38d03a38652e7
sbrijim <118214711+sbrijim@users.noreply.github.com>
2022-12-07 19:48:21 +0100

    Update main.java

--- examenEntorno/src/examenEntorno/main.java
+++ examenEntorno/src/examenEntorno/main.java
@@ -3,7 +3,7 @@ package examenEntorno;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola me llamo");
+		System.out.println("Hola me llamo Sergio");
 		System.out.println("Hasta Luego");
 	}
 }
